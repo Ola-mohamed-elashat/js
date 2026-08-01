7c9ae7cce7ee7f89247f5e1b91ecd5d995f2f9c9
olam74272-netizen <olam74272@gmail.com>
2026-08-01 16:08:47 +0300

    Finish lesson 9 assignment

--- T-01-09/main.js
+++ T-01-09/main.js
@@ -3,14 +3,27 @@ document.querySelector("h1").style.color = "red";
 document.querySelector("h1").style.fontSize = "80px";
 document.querySelector("h1").style.fontWeight = "bold";
 document.querySelector("h1").style.textAlign = "center";
-document.querySelector("h1").style.fontFamily="Arial";
+document.querySelector("h1").style["font-family"]="Arial";
 
 //   task 5
 console.table(["ahmed", "ola", "sara", "fatma"]);
-//   task 3,4
+//   task 3,
 console.log(" elzero %cweb %cschool" , "color: red; font-size: 40px;" , "color: blue; font-size: 40px;");
 //   task 6
 console.log("Iam In Console");
 // document.write("Iam In Page");
+// task 4
+console.group("Group 1");
 
+console.log("Message One");
+console.log("Message Two");
+console.group("Child Group");
+
+console.log("Message One");
+console.log("Message Two");
+console.log("Message Two");
+
+console.groupEnd(); 
+console.groupEnd(); 
+console.log("ennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnd");
 
