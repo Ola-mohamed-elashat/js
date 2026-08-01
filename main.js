07351c29cb72f37ac67b6f9d37ac8e4d55ea8211
olam74272-netizen <olam74272@gmail.com>
2026-08-01 14:12:55 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+document.querySelector("h1").style.color = "red";
+document.querySelector("h1").style.fontSize = "80px";
+document.querySelector("h1").style.fontWeight = "bold";
+document.querySelector("h1").style.textAlign = "center";
+document.querySelector("h1").style.fontFamily="Arial";
+
+
+console.table(["ahmed", "ola", "sara", "fatma"]);
+console.log(" elzero %cweb %cschool" , "color: red; font-size: 40px;" , "color: blue; font-size: 40px;");
+console.log("Iam In Console");
+document.write("Iam In Page");
