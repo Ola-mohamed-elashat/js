aa1789dd0a05106841f5337798b87b52a25bbabb
olam74272-netizen <olam74272@gmail.com>
2026-08-01 14:44:50 +0300

    Changes to be committed

--- T-01-09/main.js
+++ T-01-09/main.js
@@ -1,4 +1,4 @@
-
+// task 1
 document.querySelector("h1").style.color = "red";
 document.querySelector("h1").style.fontSize = "80px";
 document.querySelector("h1").style.fontWeight = "bold";
@@ -10,3 +10,5 @@ console.table(["ahmed", "ola", "sara", "fatma"]);
 console.log(" elzero %cweb %cschool" , "color: red; font-size: 40px;" , "color: blue; font-size: 40px;");
 console.log("Iam In Console");
 document.write("Iam In Page");
+
+
