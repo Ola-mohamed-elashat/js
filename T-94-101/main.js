cf5d72b265cf89c6e10e613b3584d8da11b9feaa
olam74272-netizen <olam74272@gmail.com>
2026-08-25 17:43:14 +0300

    Finish tasks T-64-101

---
+++ T-94-101/main.js
@@ -0,0 +1,9 @@
+// task2
+let add = document.querySelector(".classes-to-add");
+let remove = document.querySelector(".classes-to-remove");
+let Current = document.querySelector(".current");
+let result = document.querySelector(".classes-list div");
+console.log(Current.classList);
+function showClasses() {
+console.log(Current.classList);
+}
