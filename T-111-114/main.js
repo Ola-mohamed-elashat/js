eb34c7c466c84f57c4f1fe3314f3fa1f2a09b612
olam74272-netizen <olam74272@gmail.com>
2026-08-30 12:41:59 +0300

    Finish tasks T-111-114

---
+++ T-111-114/main.js
@@ -0,0 +1,59 @@
+// task1
+let font = document.getElementById("font");
+let color = document.getElementById("color");
+let size = document.getElementById("size");
+let text = document.getElementById("text");
+
+let savefont = localStorage.getItem("font")
+let savecolor = localStorage.getItem("color")
+let saveSize = localStorage.getItem("size")
+
+text.style.fontFamily = "Open Sans"
+text.style.color = "red";
+text.style.fontSize = "16px";
+font.onchange = function () {
+    text.style.fontFamily = font.value
+    localStorage.setItem("font", font.value)
+};
+color.onchange = function () {
+    text.style.color = color.value
+    localStorage.setItem("color", color.value)
+};
+size.onchange = function () {
+    text.style.fontSize = size.value + "px"
+    localStorage.setItem("size", size.value)
+};
+if (savefont !== null) {
+    text.style.fontFamily = savefont;
+    font.value = savefont;
+}
+if (savecolor !== null) {
+    text.style.color = savecolor;
+    color.value = savecolor;
+}
+if (saveSize !== null) {
+    text.style.fontSize = saveSize + "px";
+    size.value = saveSize;
+};
+// task2
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let age = document.getElementById("age");
+let country = document.getElementById("country");
+name.oninput = function () {
+    sessionStorage.setItem("name", name.value)
+};
+email.oninput = function () {
+    sessionStorage.setItem("email", email.value)
+};
+age.oninput = function () {
+    sessionStorage.setItem("age", age.value)
+};
+country.oninput = function () {
+    sessionStorage.setItem("country", country.value)
+};
+
+name.value = sessionStorage.getItem("name") || "";
+email.value = sessionStorage.getItem("email") || "";
+age.value = sessionStorage.getItem("age") || "";
+country.value = sessionStorage.getItem("country") || "";
