89893fded93c30a1124d3a450ddb52a79ef8c030
olam74272-netizen <olam74272@gmail.com>
2026-09-16 16:14:59 +0300

    Finish tasks All-Course

--- T-179-188/main.js
+++ T-179-188/main.js
@@ -78,12 +78,12 @@ fetch("data.json")
     .then((response) => response.json())
     .then((data) => {
         const num = data.slice(0, 5);
-    num.forEach((article) => {
-        dataDiv.innerHTML += `
+        num.forEach((article) => {
+            dataDiv.innerHTML += `
 <div>
 <h2>${article.title}</h2>
 <p>${article.description}</p>
 </div>
    `;
-    });
+        });
     });
